d9849534daf4075b6e53f69ffbf43a04338a9779
Dennis Thomasen <dennisthomasen@gmail.com>
2013-07-03 11:44:30 +0200

    Sliding menu added

--- src/main/java/dk/dthomasen/aarhus/MainActivity.java
+++ src/main/java/dk/dthomasen/aarhus/MainActivity.java
@@ -2,14 +2,61 @@ package dk.dthomasen.aarhus;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends Activity
 {
+    protected final String TAG = this.getClass().getName();
+    private SlidingMenu slidingMenu;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        slidingMenu = new SlidingMenu(this);
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+        slidingMenu.setShadowDrawable(R.drawable.slidingmenu_shadow);
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        slidingMenu.setFadeDegree(0.35f);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidingMenu.setMenu(R.layout.slidingmenu);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if ( slidingMenu.isMenuShowing()) {
+            slidingMenu.toggle();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ( keyCode == KeyEvent.KEYCODE_MENU ) {
+            this.slidingMenu.toggle();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.slidingMenu.toggle();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ src/main/java/dk/dthomasen/slidingmenu/SlidingMenuFragment.java
@@ -0,0 +1,81 @@
+package dk.dthomasen.slidingmenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import dk.dthomasen.aarhus.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlidingMenuFragment extends Fragment implements ExpandableListView.OnChildClickListener {
+   
+    private ExpandableListView sectionListView;
+   
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+       
+        List<Section> sectionList = createMenu();
+               
+        View view = inflater.inflate(R.layout.slidingmenu_fragment, container, false);
+        this.sectionListView = (ExpandableListView) view.findViewById(R.id.slidingmenu_view);
+        this.sectionListView.setGroupIndicator(null);
+       
+        SectionListAdapter sectionListAdapter = new SectionListAdapter(this.getActivity(), sectionList);
+        this.sectionListView.setAdapter(sectionListAdapter);
+       
+        this.sectionListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+              @Override
+              public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                return true;
+              }
+            });
+       
+        this.sectionListView.setOnChildClickListener(this);
+       
+        int count = sectionListAdapter.getGroupCount();
+        for (int position = 0; position < count; position++) {
+            this.sectionListView.expandGroup(position);
+        }
+       
+        return view;
+    }
+
+    private List<Section> createMenu() {
+        List<Section> sectionList = new ArrayList<Section>();
+
+        return sectionList;
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v,
+            int groupPosition, int childPosition, long id) {
+
+        switch ((int)id) {
+        case 101:
+            //TODO
+            break;
+        case 102:
+            //TODO
+            break;
+        case 201:
+            //TODO
+            break;
+        case 202:
+            //TODO
+            break;
+        case 203:
+            //TODO
+            break;
+        case 204:
+            //TODO
+            break;
+        }
+       
+        return false;
+    }
+}
